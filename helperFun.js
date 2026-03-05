a1f917704e5888c0ee9703981eaf8fbb5501afc0
Freak644 <hoodafreak@gmail.com>
2026-03-05 17:11:41 +0530

    complete weather UI

--- helperFun.js
+++ helperFun.js
@@ -37,16 +37,16 @@ export const getWeatherData = async (api_ke,city)=>{
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_ke}&units=metric`
     let rqst = await fetch(url);
     let data = await rqst.json();
+    console.log(data)
     ShowData(data)
 }
 
 function ShowData(data) {
         let divs=document.getElementById('dtemp').children
-        let wind=document.getElementById('curson')
+        let wind=document.getElementById('curson').children
         let speed=document.getElementById('speed')
         let image=document.createElement('img')
-        let hor = new Date().getHours();
-        console.log(divs,wind,speed,image)
+        let hor = new Date().getHours()
         if (divs[0].children.length==1) {
             divs[0].children[0].remove();
         }
@@ -57,14 +57,22 @@ function ShowData(data) {
          image.src=`https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
       }
       divs[0].append(image);
-      divs[1].innerHTML=Math.round(data.main.temp)+'°'
-    //   divs[2].innerHTML= new Date((data.sys.sunrise+data.timezone)* 1000).toLocaleTimeString();
-      divs[2].innerHTML=Math.round(data.main.temp_max)+'°'
-      divs[3].innerHTML=Math.round(data.main.temp_min)+'°'
-      divs[4].innerHTML=data.name;
-      speed.innerHTML='Speed: '+data.wind.speed;
-      wind.style.transform=`rotate(${data.wind.deg}deg)`
-      divs[5].innerHTML=data.weather[0].description.toUpperCase();
+      divs[1].innerHTML=Math.round(data.main.temp);
+      let span = document.createElement("span")
+      span.innerText="℃"
+      divs[1].append(span)
+      divs[2].innerHTML = "📍" + data.name
+      
+      divs[3].innerHTML= data.weather[0].description.toUpperCase();
+      speed.innerHTML='Speed: '+data.wind.speed + "KMh";
+      const directions = [
+        "N","NE","E","SE","S","SW","W","NW"
+        ];
+        const index = Math.round(data.wind.deg / 45) % 8;
+        let newSpan = document.createElement("span");
+        newSpan.innerText = directions[index];
+        wind[0].parentElement.append(newSpan)
+        wind[0].style.transform=`rotate(${data.wind.deg}deg)`
     }
 
 
