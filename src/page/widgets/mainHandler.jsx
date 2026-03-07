7e31ad92c80f27d96f033782daef4a0b0549f0dd
Freak644 <hoodafreak@gmail.com>
2026-03-07 23:01:31 +0530

    Import clock from HTML :

--- src/page/widgets/mainHandler.jsx
+++ src/page/widgets/mainHandler.jsx
@@ -1,8 +1,10 @@
+import DigitClock from "./components/clock/clockmgmt";
+
 export default function Widgets({data}) {
     
     return(
         <div className="thonePrincess z-10 bg-transparent">
-            
+            <DigitClock/>
         </div>
     )
 }
