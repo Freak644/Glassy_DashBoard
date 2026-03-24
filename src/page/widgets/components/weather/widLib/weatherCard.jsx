74718d7da58a43344a30879edee24eed7ef8888d
Freak644 <hoodafreak@gmail.com>
2026-03-24 17:48:37 +0530

    complete weather card

--- src/page/widgets/components/weather/widLib/weatherCard.jsx
+++ src/page/widgets/components/weather/widLib/weatherCard.jsx
@@ -12,6 +12,7 @@ export default function WeatherCard() {
     const featchWeather = async (city, api_key) => {
         let rqst = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}&units=metric`)
         let data = await rqst.json();
+        console.log(data)
         setData(data);
     }
 
@@ -35,7 +36,7 @@ export default function WeatherCard() {
         <div className="underTaker">
             {myData?.city.length === 0 ? <AskCity crntData={myData} /> :
             <>
-                {Object.keys(weatherData).length === 0 ? <div className="Loader"></div> :
+                {Object.keys(weatherData).length === 0 ? <div className="loader"></div> :
                 <>
                 <div className="imgDiv h-25 w-25 absolute top-1 left-1">
                     <img src={`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`} alt="" />
@@ -43,14 +44,19 @@ export default function WeatherCard() {
 
                 <p className="absolute left-27 top-3.5 font-bold text-4xl">{Math.round(weatherData.main.temp)}<span className="text-lg absolute top-0">℃</span></p>
                 <p className="absolute p-1 w-50 right-1 flex items-center flex-col top-5.5 text-lg font-semibold">{`${"📍"+weatherData.name}`} <span className="text-[15px] text-gray-400!">{weatherData.weather[0].description.toUpperCase()}</span></p>
-                <div className="border border-amber-400 h-30 relative top-12 w-full">
+                <div className=" h-30 relative top-12 w-full left-1/7">
                     <span className="absolute top-1 left-2.5 font-semibold text-[14px]">Wind🍃</span>
-                    <div className="flex items-center flex-col p-1">
+                    <div className="flex items-center flex-col p-1 gap-2.5 absolute top-8 left-2.5">
                         <img src={CursorImg} className="h-10! w-10!"
                         style={{
                             transform: `rotate(${Math.round(weatherData.wind.deg)}deg)`
                         }} alt="" />
                         {getDirection()}
+                    </div>
+
+                    <div className="flex items-center flex-col gap-2 top-1.5  w-25 left-2/5 absolute">
+                        <img src={windGif} className="h-18! w-15!" alt="" />
+                        <span>Speed💨 {weatherData.wind.speed}</span>
                     </div>
                 </div>
                 </>}
