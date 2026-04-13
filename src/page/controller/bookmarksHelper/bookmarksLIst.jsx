4f2adc1d9a731ec565e08129b9475e4f2bbd2ef9
Gabimaru <hoodafreak@gmail.com>
2026-04-13 19:24:23 +0530

    Update package / start building bookmarks Manager

--- src/page/controller/bookmarksHelper/bookmarksLIst.jsx
+++ src/page/controller/bookmarksHelper/bookmarksLIst.jsx
@@ -1,15 +1,25 @@
+import { useEffect } from "react";
 import { database } from "../../../lib/globalState"
 
 export default function BookmarkList() {
     const bookList = database(stat=> stat.db.bookmarks);
 
+    // useEffect(()=>{
+    //     Object.entries(bookList).map(([category, itmes]) => {
+    //                 console.log(category,itmes)
+    //             })
+    // },[])
     return(
-        <div className="underTaker ">
+        <div className="underTaker items-start!">
+            <div className="searchBox w-[98%] shadow shadow-black/25 rounded-lg h-25 relative top-0
+            mt-2.5">
+                <input type="text" placeholder="Type To Search..." className="h-10 pl-3 rounded-lg w-3/5 border shadow shadow-amber-50/20" />
 
+            </div>
             {
-                Object.entries(bookList).map(([category, itmes]) => {
-                    console.log(category,itmes)
-                })
+                // Object.entries(bookList).map(([category, itmes]) => {
+                //     console.log(category,itmes)
+                // })
             }
         </div>
     )
